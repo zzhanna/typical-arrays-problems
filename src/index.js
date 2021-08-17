113f52d82865fbcefd3a3a0b4a119f3ee7817ef3
zzhanna <foto1drug@gmail.com>
2021-08-17 02:44:31 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        let minArray = array.sort((a, b) => a - b);
+        return minArray[0];
+    } else return `0`;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        let maxArray = array.sort((a, b) => a - b);
+        return maxArray[array.length - 1];
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        let sum = array.reduce((a, b) => a + b);
+        return sum / array.length;
+    } else return 0;
 }
